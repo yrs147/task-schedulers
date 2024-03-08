f7f37b28982a17231bbbed6dc57c0b33b4c51ee8
Yash Raj <yashraj14700728@gmail.com>
2024-03-08 15:13:57 +0530

    Added scheduler methods

--- scheduler.py
+++ scheduler.py
@@ -3,3 +3,22 @@ import time
 import random
 from datetime import datetime, timedelta
 from task_scheduler.db import Task
+import schedule
+
+def execute_task(task_id,task_name):
+    print(f"Executing Task {task_id}: {task_name}")
+    time.sleep(random.randint(1,6))
+    print(f"Task {task_id}: {task_name} completed")
+
+def task_scheduler():
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')    
+    tasks_to_execute = Task.select().where(Task.execution_time <=current_time)
+
+    for task in tasks_to_execute:
+        task_id,task_name = task.id, task.name
+ 
+
+def schedule_tasks():
+    while True:
+        schedule.run_pending()  
+        time.sleep(1)     
